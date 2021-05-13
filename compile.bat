c93e1ab83243c615c083f74a81fed0210a181f5b
Mitsutaka Okazaki <okaxaki@users.noreply.github.com>
2021-05-13 11:21:32 +0900

    Suppress intel HEX output

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
-zasm64 -Cpb3psg.com src\pb3psg.asm
-zasm64 -Cpb3fm.com src\pb3fm.asm
+zasm64 -Z -Cpb3psg.com src\pb3psg.asm
+zasm64 -Z -Cpb3fm.com src\pb3fm.asm
 
 bincut2 -o pb3fm.bin -l 200 pb3fm.com
 bincut2 -o pb3psg.bin -l 200 pb3psg.com
